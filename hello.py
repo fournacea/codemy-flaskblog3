80cef1e13e77275d12d4b6bd796638031011f8f1
Adam Fournace <fournacea@gmail.com>
2023-03-19 13:11:48 -0400

    Add navbar

--- hello.py
+++ hello.py
@@ -20,7 +20,7 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
+def server_error(e):
     return render_template("500.html"), 500
 
 
