36ef351ec57dc3336974c722b6df3e107c81ac10
Adam Fournace <fournacea@gmail.com>
2023-03-19 13:48:12 -0400

    Add name form page

--- hello.py
+++ hello.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+
 
 #Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Super duper secret key"
+
+
+#Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 #Create a Route Decorator
 @app.route('/')
@@ -12,6 +24,16 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form =NamerForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form=form)
 
 
 #Error Pages
