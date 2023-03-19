6f5771914f486bc1a62089a302c73d2c2d291524
Adam Fournace <fournacea@gmail.com>
2023-03-19 12:31:31 -0400

    Add error pages 404 and 500

--- hello.py
+++ hello.py
@@ -14,5 +14,15 @@ def user(name):
     return render_template('user.html', name=name)
 
 
+#Error Pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
