f28619d59d6820e1a8b084176584344e85e8a9ce
Adam Fournace <fournacea@gmail.com>
2023-03-20 15:38:16 -0400

    ADD flask flash, custom css/js/imgs

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
@@ -33,6 +33,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash(f"Hello, { name }! Form was submitted successfully.")
     return render_template("name.html", name=name, form=form)
 
 
